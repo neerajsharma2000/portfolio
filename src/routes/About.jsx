a9db25b24581f8f536aeaf977471ca83d8003d33
neerajsharma <nskool.2000@gmail.com>
2023-01-05 16:33:56 +0530

    responsive impov

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,63 +1,91 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Navbar } from '../components/Navbar';
-import Palette from '../components/Palette';
-import { FiArrowRightCircle } from 'react-icons/fi';
-import bg from '../assets/mohammad-rahmani-LrxSl4ZxoRs-unsplash.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+import Palette from "../components/Palette";
+import { FiArrowRightCircle } from "react-icons/fi";
+import bg from "../assets/mohammad-rahmani-LrxSl4ZxoRs-unsplash.jpg";
 
 const About = () => {
   return (
-    <div style={{ backgroundImage: `url(${bg})` }}  className='w-full h-screen flex flex-col justify-center items-center bgpic'>
+    <div
+      style={{ backgroundImage: `url(${bg})` }}
+      className="container flex flex-col justify-center items-center bgpic"
+    >
       {/* <Navbar /> */}
-      <div className='w-full h-screen flex flex-col justify-center items-center  text-blue-400  '>
-
-       
-
-        <div className="max-w-[1000px]  mx-auto p-2 flex-col md:flex  bg-black opacity-80">
+      <div className=" flex flex-col justify-center items-center  text-blue-400  ">
+        <div className="max-w-[1000px]   flex-col md:flex  bg-black opacity-80">
           <div className="flex justify-center items-center ">
-            <h1 className="text-2xl md:text-4xl md:font-extrabold hover:bg-blue-400 hover:text-black rounded-md">About</h1>
+            <h1 className="text-2xl md:text-3xl font-extrabold hover:bg-blue-400 hover:text-black rounded-md">
+              About
+            </h1>
           </div>
-          <div className="sm:text-left text-2xl  flex justify-center items-center">
-            <p>I am Neeraj Sharma, B.Tech. C.S.E. Graduate(2022 passout) from M.M.M.U.T, Gorakhpur.Currently a Software developer(Java,Springboot,Bootstrap,MySQL). I have good problem solving skills and have programming language experience in C++, Java as well as hands-on experience on data science and Springboot projects.
+          <div className="sm:text-left md:text-2xl  flex justify-center items-center">
+            <p>
+              I am Neeraj Sharma, B.Tech. C.S.E. Graduate(2022 passout) from
+              M.M.M.U.T, Gorakhpur.Currently a Software
+              developer(Java,Springboot,Bootstrap,MySQL). I have good problem
+              solving skills and have programming language experience in C++,
+              Java as well as hands-on experience on data science and Springboot
+              projects.
             </p>
           </div>
         </div>
 
         <div className="max-w-[1000px]  flex-col justify-center items-center  bg-black opacity-80">
           <div className="flex-col justify-center items-center">
-            <h1 className="text-2xl md:text-3xl md:font-extrabold">Chaipoint| Java, Springboot,MySQL</h1>
-            <p><FiArrowRightCircle size={10} /> Worked and developed multiple APIs under the 7-member developer team for MyChaiCorner organization
-              involving the use of SpringBoot,Vaadin, JAVA, Rest APIs, etc. Also tested, integrated, debugged, monitored,
-              and deployed these APIs eg-store tags, filter by tags, terminal creation, bank details tab layout.Optimized and fixed
-              bugs of multiple features related to the MyChaiCorner website.</p>
-            <p><FiArrowRightCircle size={10} />Collaborated with marketing team and built Enchante Instagram filters with Meta spark player and received
-              ’ultimate team-player’ award from the team.</p></div>
+            <h1 className="text-2xl md:text-3xl font-extrabold">
+              Chaipoint| Java, Springboot,MySQL
+            </h1>
+            <p>
+              <FiArrowRightCircle size={10} /> Worked and developed multiple
+              APIs under the 7-member developer team for MyChaiCorner
+              organization involving the use of SpringBoot,Vaadin, JAVA, Rest
+              APIs, etc. Also tested, integrated, debugged, monitored, and
+              deployed these APIs eg-store tags, filter by tags, terminal
+              creation, bank details tab layout.Optimized and fixed bugs of
+              multiple features related to the MyChaiCorner website.
+            </p>
+            <p>
+              <FiArrowRightCircle size={10} />
+              Collaborated with marketing team and built Enchante Instagram
+              filters with Meta spark player and received ’ultimate team-player’
+              award from the team.
+            </p>
+          </div>
           <div className="flex-col justify-center items-center">
-            <h1 className="text-2xl md:text-3xl md:font-extrabold">EISystems Services| Python, Flask,Bootstrap</h1>
-            <p><FiArrowRightCircle size={10} />Individually built 2 data science projects namely-Stroke disease prediction, and IPL Twitter sentiment
-              analysis using machine learning datasets. Cleaned, Preprocessed, implemented, tested, and deployed the project
-              on Heroku platform.</p>
-            <p><FiArrowRightCircle size={10} />Accuracy scores were above 80 percent.</p></div>
+            <h1 className="text-2xl md:text-3xl font-extrabold">
+              EISystems Services| Python, Flask,Bootstrap
+            </h1>
+            <p>
+              <FiArrowRightCircle size={10} />
+              Individually built 2 data science projects namely-Stroke disease
+              prediction, and IPL Twitter sentiment analysis using machine
+              learning datasets. Cleaned, Preprocessed, implemented, tested, and
+              deployed the project on Heroku platform.
+            </p>
+            <p>
+              <FiArrowRightCircle size={10} />
+              Accuracy scores were above 80 percent.
+            </p>
+          </div>
           <div className="flex-col justify-center items-center">
-            <h1 className="text-2xl md:text-3xl md:font-extrabold">Bachelor of Technology,C.S.E</h1>
+            <h1 className="text-2xl md:text-3xl font-extrabold">
+              Bachelor of Technology,C.S.E
+            </h1>
             <p>Madan Mohan Malaviya University of Technology,Gorakhpur</p>
-            <p>2018-2022</p></div>
+            <p>2018-2022</p>
+          </div>
           <div className="flex-col justify-center items-center">
-            <h1 className="text-2xl md:text-3xl md:font-extrabold">High School</h1>
+            <h1 className="text-2xl md:text-3xl font-extrabold">High School</h1>
             <p>Gyandeep Academy</p>
-            <p>2016-2017</p></div>
-
-
+            <p>2016-2017</p>
+          </div>
         </div>
-
       </div>
 
-
       <Palette />
+    </div>
+  );
+};
 
-    </div >
-
-  )
-}
-
-export default About
+export default About;
--- src/routes/Experience.jsx
+++ src/routes/Experience.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Navbar } from '../components/Navbar';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
 import AWS from "../assets/aws.png";
 import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
@@ -19,59 +19,83 @@ import heroku from "../assets/heroku.png";
 import Flask from "../assets/Flask.png";
 import python from "../assets/python.png";
 
-
-
-
-
-import Palette from '../components/Palette';
-import bg from '../assets/mohammad-rahmani-LrxSl4ZxoRs-unsplash.jpg';
-
-
+import Palette from "../components/Palette";
+import bg from "../assets/mohammad-rahmani-LrxSl4ZxoRs-unsplash.jpg";
 
 const Experience = () => {
   return (
-    <div style={{backgroundImage:`url(${bg})`}} className='w-full h-screen flex flex-col justify-center items-center bgpic'>
+    <div
+      style={{ backgroundImage: `url(${bg})` }}
+      className="w-full h-screen flex flex-col justify-center items-center bgpic"
+    >
       {/* <Navbar /> */}
 
-      <div className='w-full h-screen flex flex-col justify-center items-center  text-blue-400 '>
+      <div className="w-full h-screen flex flex-col justify-center items-center  text-blue-400 ">
         <div className="w-max-[1000px]  mx-auto p-4 flex justify-center items-center  bg-black opacity-80 ">
-          <h1 className='text-2xl md:text-4xl md:font-bold rounded-md hover:bg-blue-400 hover:text-black'>Experience</h1>
-
+          <h1 className="text-2xl md:text-4xl md:font-bold rounded-md hover:bg-blue-400 hover:text-black">
+            Skills
+          </h1>
         </div>
-        <div className="w-max-[1000px]   mx-auto  flex p-2 bg-black opacity-80">
-          <div className="flex mx-4 justify-center items-center  w-20 h-20"><img src={react} alt="React" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={HTML} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={CSS} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={Javascript} alt="" /></div>
+        <div className="w-max-[1000px]   flex bg-black opacity-80">
+          <div className="flex mx-4 justify-center items-center  w-20 h-20">
+            <img src={react} alt="React" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={HTML} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={CSS} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={Javascript} alt="" />
+          </div>
         </div>
-        <div className="w-max-[1000px]   mx-auto  flex p-2  bg-black opacity-80">
-
-        <div className="flex mx-4 justify-center items-center  w-20 h-20"><img src={cpp} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={java} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={mysql} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={spboot} alt="" /></div>
-         </div>
-         <div className="w-max-[1000px]   mx-auto  flex p-2  bg-black opacity-80">
- 
-         <div className="flex mx-4 justify-center items-center  w-20 h-20"><img src={bkt} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={jnkns} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={jira} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={heroku} alt="" /></div>
-         </div>
-         <div className="w-max-[1000px]   mx-auto  flex p-2  bg-black opacity-80">
- 
-         <div className="flex mx-4 justify-center items-center  w-20 h-20"><img src={python} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={Flask} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center  w-20 h-20"><img src={AWS} alt="" /></div>
-          <div className="flex mx-4 justify-center items-center w-20 h-20"><img src={Tailwind} alt="" /></div>
-        
+        <div className="w-max-[1000px]   flex  bg-black opacity-80">
+          <div className="flex mx-4 justify-center items-center  w-20 h-20">
+            <img src={cpp} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={java} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={mysql} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={spboot} alt="" />
+          </div>
         </div>
-      
-
+        <div className="w-max-[1000px]   flex  bg-black opacity-80">
+          <div className="flex mx-4 justify-center items-center  w-20 h-20">
+            <img src={bkt} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={jnkns} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={jira} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={heroku} alt="" />
+          </div>
+        </div>
+        <div className="w-max-[1000px]   flex  bg-black opacity-80">
+          <div className="flex mx-4 justify-center items-center  w-20 h-20">
+            <img src={python} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={Flask} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center  w-20 h-20">
+            <img src={AWS} alt="" />
+          </div>
+          <div className="flex mx-4 justify-center items-center w-20 h-20">
+            <img src={Tailwind} alt="" />
+          </div>
+        </div>
       </div>
-      <Palette /> 
+      <Palette />
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
